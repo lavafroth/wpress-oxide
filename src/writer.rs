27fbc67ef2f00614f178c03764f37eecfbb110e1
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2023-03-26 17:50:49 +0530

    Creates and uses custom ArchiveError enum for errors related to file writes.

--- src/writer.rs
+++ src/writer.rs
@@ -1,9 +1,14 @@
-use crate::common::{Header, EOF_BLOCK};
+use crate::{
+    common::{Header, EOF_BLOCK},
+    FileParseError,
+};
 use std::{
     fs::File,
     io::{copy, Write},
     path::{Path, PathBuf},
 };
+
+use thiserror::Error;
 
 /// Structure to write multiple files and corresponding metadata into a wpress archive.
 pub struct Writer {
@@ -11,11 +16,25 @@ pub struct Writer {
     paths: Vec<PathBuf>,
 }
 
+#[derive(Debug, Error)]
+pub enum ArchiveError {
+    #[error("failed to create archive file: {0}")]
+    FileCreation(std::io::Error),
+    #[error("failed to add file entry to archive: {0}")]
+    EntryAddition(std::io::Error),
+    #[error("failed to traverse and recursively add files to archive: {0}")]
+    DirectoryTraversal(std::io::Error),
+    #[error("{0}")]
+    FileParse(#[from] FileParseError),
+    #[error("failed writing to archive: {0}")]
+    FileWrite(std::io::Error),
+}
+
 impl Writer {
     /// Creates a new `Writer` with the destination being the path supplied.
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Writer, Box<dyn std::error::Error>> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Writer, ArchiveError> {
         Ok(Writer {
-            file: File::create(path)?,
+            file: File::create(path).map_err(ArchiveError::FileCreation)?,
             paths: vec![],
         })
     }
@@ -23,14 +42,14 @@ impl Writer {
     /// Lazily adds paths to the `Writer`. It merely tells the `Writer` to note the supplied path
     /// and does not write to the underlying file. To write to the underlying file, use the
     /// `write` method after `add`ing all the files.
-    pub fn add<P: AsRef<Path>>(&mut self, path: P) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn add<P: AsRef<Path>>(&mut self, path: P) -> Result<(), ArchiveError> {
         let path = path.as_ref();
         // If the given path is a directory,
         // recursively add all the files and
         // subdirectories inside it.
         if path.is_dir() {
-            for entry in path.read_dir()? {
-                self.add(entry?.path())?;
+            for entry in path.read_dir().map_err(ArchiveError::EntryAddition)? {
+                self.add(entry.map_err(ArchiveError::EntryAddition)?.path())?;
             }
         } else if path.is_file() {
             self.paths.push(path.to_path_buf());
@@ -42,16 +61,19 @@ impl Writer {
     /// Writes header structures and associated data to the underlying file handle. Since the
     /// object is consumed, the file is closed on drop, making sure we cannot incorrectly write
     /// multiple times to the same file.
-    pub fn write(mut self) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn write(mut self) -> Result<(), ArchiveError> {
         for path in self.paths.iter() {
             let header = Header::from_file(path)?;
-            println!("{:?}", header);
-            let mut handle = File::open(path)?;
-            self.file.write_all(&header.bytes)?;
-            copy(&mut handle, &mut self.file)?;
+            let mut handle = File::open(path).map_err(FileParseError::FileRead)?;
+            self.file
+                .write_all(&header.bytes)
+                .map_err(ArchiveError::FileWrite)?;
+            copy(&mut handle, &mut self.file).map_err(ArchiveError::FileWrite)?;
         }
         // This marks the end of the file.
-        self.file.write_all(EOF_BLOCK)?;
+        self.file
+            .write_all(EOF_BLOCK)
+            .map_err(ArchiveError::FileWrite)?;
         Ok(())
     }
 
